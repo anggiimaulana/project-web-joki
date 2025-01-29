f2d21ef88fc470f545a3a82c56af5a3fe93c12dc
anggii <anggabos120@gmail.com>
2025-01-29 15:54:34 -0400

    FaqPage

---
+++ app/Http/Controllers/FaqController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FaqController extends Controller
+{
+    public function index()
+    {
+        $slug = 'faq';
+        $title = 'FAQ JOKIINIT.com';
+        return view('pages.faq', compact('slug', 'title'));
+    }
+}
